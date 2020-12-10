8ed8ebd990045be59071cd27722c88999278b2e6
Arun Tekurala <tekurala.arun@gmail.com>
2020-12-10 17:53:44 -0500

    Modified app js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         // Edit <code>src/App.js</code> and save to reload.
 		 Welcome!! Veena Gangi
         </p>
         <a
